8a7c16a7ca9d749c1bdcf1c1f6551d814308dab7
Ruoxin Huang <cs100wcu@ieng6-202.ucsd.edu>
2017-01-26 22:49:48 -0800

    perform test

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include<fstream>
 #include<iostream>
 #include<string>
 #include<algorithm>
@@ -9,6 +10,92 @@
 #include "DictionaryHashtable.h"
 #define LETTERS 26
 using namespace std;
+
+int testFind() {
+  //Initialize words
+  vector<std::string> words;
+  vector<string>::iterator wit;
+  vector<string>::iterator wen;
+  //initialize nonwords
+  set<string> nope;
+  set<string>::iterator nit;
+  set<string>::iterator nen;
+
+  //Initialize data structures
+  DictionaryBST d_bst;
+  DictionaryHashtable d_ht;
+  DictionaryTrie dt;
+  int t_bst, t_ht, tt;
+
+  words.push_back("harry");
+  words.push_back("sriram");
+  words.push_back("cse");
+  words.push_back("crucio");
+  words.push_back("autocomplete");
+
+  cout << "Inserting into Dictionaries..." << endl;
+
+  wit = words.begin();
+  wen = words.end();
+
+  for(; wit != wen; ++wit)
+    {
+      cout << "Inserting: \"" << *wit << "\"... ";
+      t_bst = d_bst.insert(*wit);
+      t_ht = d_ht.insert(*wit);
+      tt = dt.insert(*wit, 1);
+      //cout << t_bst << " " << t_ht << " "<< tt << "... ";
+      if(!t_bst)
+        {
+          cout << "failed for DictionaryBST... ";
+        }
+      if(!t_ht)
+        {
+          cout << "failed for DictionaryHashset... ";
+        }
+      if(!tt)
+        {
+          cout << "failed for DictionaryTrie... ";
+        }
+      if(t_bst && t_ht && tt)
+        {
+          cout << "PASSED! :D ";
+        }
+      cout << endl;
+    }
+  cout << endl;
+  
+  cout << "Finding words in Dictionaries..." << endl;
+
+  wit = words.begin();
+  wen = words.end();
+  for(; wit != wen; ++wit)
+    {
+      cout << "Finding: \"" << *wit << "\"... ";
+      t_bst = d_bst.find(*wit);
+      t_ht = d_ht.find(*wit);
+      tt = dt.find(*wit);
+      if(t_bst)
+        {
+          cout << "failed for DictionaryBST... ";
+        }
+      if(t_ht)
+        {
+          cout << "failed for DictionaryHashset... ";
+        }
+      if(tt)
+        {
+          cout << "failed for DictionaryTrie... ";
+        }
+      if(!t_bst && !t_ht && !tt)
+        {
+          cout << "PASSED! :D ";
+        }
+      cout << endl;
+    }
+
+  return 0;
+}
 
 
 int main(int argc, char** argv)
@@ -35,11 +122,11 @@ int main(int argc, char** argv)
   words.push_back("crucio");
   words.push_back("autocomplete");
   
-  
   cout << "Inserting into Dictionaries..." << endl;
 
   wit = words.begin();
   wen = words.end();
+
   for(; wit != wen; ++wit)
     {
       cout << "Inserting: \"" << *wit << "\"... ";
@@ -94,11 +181,9 @@ int main(int argc, char** argv)
         }
       cout << endl;
     }
-
   cout << endl;
-
-  
 /*You are supposed to add more test cases in this file */
   
+  testFind();
   return 0;
 }
