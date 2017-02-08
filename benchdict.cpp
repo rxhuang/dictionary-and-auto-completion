7084a43508e741bc2a47110496aed2b2e374e6bd
Muyang Wu <muw011@ieng6-201.ucsd.edu>
2017-02-08 15:22:27 -0800

    finished dictBST

--- benchdict.cpp
+++ benchdict.cpp
@@ -8,6 +8,8 @@
 #include "DictionaryTrie.h"
 #include "DictionaryBST.h"
 #include "DictionaryHashtable.h"
+#include <algorithm>
+#include <iterator>
 #define LETTERS 26
 using namespace std;
 
@@ -25,49 +27,31 @@ int main(int argc, char** argv){
   int min_size =atoi(argv[1]);
   int step_size = atoi(argv[2]);
   int num_iterations = atoi(argv[3]);
-  string dictfile = argv[4];
   //check for valid input
   if (min_size<0 || step_size<0 || num_iterations <0) {
-    err<<"Invalid input"<<'\n';
+    cerr<<"Invalid input"<<'\n';
     return -1;
   }
-  //check for valid file
-  in.open(dictfile, ios::in);
-  if (!in.is_open()) {
-    std::cout<<"Invalid input file. \n";
-    return -1;
-  }
-
-  in.seekg(0, ios_base::end);
-  //check for empty file
-  unsigned int len = in.tellg();
-  if (len==0) {
-    cout<<"file is empty." << endl;
-    return -1;
-  }
-  in.seekg(0, ios_base::beg);
-  //read line by line to find out the number of words in dict
-  in.unsetf(std::ios_base::skipws);
-  unsigned word_count = std::count(istream_iterator<char>(in),istream_iterator<char>(), '\n');
-  in.close();
-
+  
   cout << "***Benchmarking dictionary BST***" << endl;
+  int size = min_size;
+  int incr = step_size;
   for(int i = 0; i < num_iterations ; i++){
     in.open(argv[4], ios::in);
     in.seekg(0, ios_base::beg);
-    DictionaryBST bst = new DictionaryBST();
-    if((int)word_count < min_size*step_size){
-      cout << "Dict size too small." << endl;
-      util.load_dict(*bst, in, word_count);
-    }
-    else
-      util.load_dict(bst, in, min_size + i*step_size);
+    DictionaryBST bst;
+    vector<string> wordsToFind;
+    util.load_dict(bst, in, min_size + i*step_size);
+    util.load_vector(wordsToFind, in, 100);
     in.close();
-    int test[num_iterations];
-    for(int i = 0; i < num_iterations ; i++){
-      timer.begin_timer();
+    vector<string> lexicon;
+    timer.begin_timer();
+    for(string w:wordsToFind){
+      bst.find(w);
     }
-
-
+    long long time = timer.end_timer();
+    cout << size << "\t" << time  << endl;
+    size += incr;
   }
+
 }
